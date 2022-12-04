d4e19dfb444fece75461da733a7ec82b7afbdbe7
Дмитрий <bazaley-dmitriy@i.ua>
2022-12-04 23:44:28 +0200

    fix

--- src/components/UpdateContact/UpdateContact.jsx
+++ src/components/UpdateContact/UpdateContact.jsx
@@ -25,7 +25,6 @@ class UpdateContact extends Component {
             name="name"
             value={this.state.name}
             onChange={this.handleChange}
-            placeholder="Name"
             pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
             title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
             required
